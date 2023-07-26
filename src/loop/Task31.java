37e4b5dad8b96c4aadb2ef26d56027d90f0d80c4
ra1kardFromMacOSDaria <91raykard@gmail.com>
2023-07-27 00:53:12 +0300

    task31 is done

---
+++ src/loop/Task31.java
@@ -0,0 +1,20 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Task31 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int num = console.nextInt();
+        int n = console.nextInt();
+        int x = num;
+        if (n != 0) {
+            for (int i = 1; i < n; i++) {
+                x *= num;
+            }
+            System.out.println(x);
+        } else {
+            System.out.println(1);
+        }
+    }
+}
