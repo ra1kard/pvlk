3999c2ce55491fb04d8057b11cfa19f061c826a9
ra1kardFromMacOSDaria <91raykard@gmail.com>
2023-07-27 01:18:14 +0300

    task32 is done

---
+++ src/loop/task32.java
@@ -0,0 +1,19 @@
+package loop;
+
+import java.util.Scanner;
+
+public class task32 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int n = console.nextInt();
+        String x = null;
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                x = "1";
+            } else {
+                x += 0;
+            }
+        }
+        System.out.println(x);
+    }
+}
