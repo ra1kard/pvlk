71c3ab797edc14741aebff0c24ab6b9c89798441
ra1kardFromMacOSDaria <91raykard@gmail.com>
2023-07-30 14:02:50 +0300

    task40 is done

---
+++ src/loop/Task40.java
@@ -0,0 +1,24 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Task40 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int n = console.nextInt();
+        int x = 1;
+        int y = 2;
+        int rsl = 0;
+        while (y < n || y == n) {
+            if (y != n) {
+                System.out.print(x + "*" + y + " + ");
+            } else {
+                System.out.print(x + "*" + y);
+            }
+            rsl += x * y;
+            x++;
+            y++;
+        }
+        System.out.println(" = " + rsl);
+    }
+}
