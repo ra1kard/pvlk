c3a5a747aa1793a08cd8848715e9b162f3c1ab79
ra1kardFromMacOSDaria <91raykard@gmail.com>
2023-07-30 15:33:05 +0300

    task44 is done

---
+++ src/loop/Task44.java
@@ -0,0 +1,32 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Task44 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int x = console.nextInt();
+        int y = console.nextInt();
+        String strX = String.valueOf(x);
+        String strY = String.valueOf(y);
+        char[] arrayX = strX.toCharArray();
+        char[] arrayY = strY.toCharArray();
+        int sumX = 0;
+        int sumY = 0;
+
+        for (int i = 0; i < strX.length(); i++) {
+            sumX += arrayX[i];
+        }
+        for (int i = 0; i < strY.length(); i++) {
+            sumY += arrayY[i];
+        }
+
+        System.out.println("sumX = " + sumX);
+        System.out.println("sumY = " + sumY);
+        if (sumX == sumY) {
+            System.out.println("Числа " + x + " и " + y + " родственные");
+        } else {
+            System.out.println("Числа " + x + " и " + y + " не родственные");
+        }
+    }
+}
