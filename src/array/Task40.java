b87fbc7b44197eb6c735cab665389da3d10e4bcf
ra1kardFromMacOSDaria <91raykard@gmail.com>
2023-08-05 17:39:31 +0300

    task40 is done

---
+++ src/array/Task40.java
@@ -0,0 +1,24 @@
+package array;
+
+public class Task40 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 5, 7};
+        //int[] array = {10, 2, 10, 17, 8, -5, 0};
+
+        int[] arrayNew = new int[array.length];
+
+        for (int i = 0; i < arrayNew.length; i++) {
+            if (i == 0) {
+                arrayNew[i] = array[array.length-1];
+            } else {
+                arrayNew[i] = array[i-1];
+            }
+        }
+
+        //по заданию не нужно выводить, но для наглядности вывожу
+        for (int i : arrayNew) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
