58a2ff8cf8d25ae7e70a64215a9746ab0259afdc
ra1kardFromMacOSDaria <91raykard@gmail.com>
2023-08-13 16:17:52 +0300

    task32 refactor

--- src/loop/Task32_2.java
+++ src/loop/Task32_2.java
@@ -2,16 +2,15 @@ package loop;
 
 import java.util.Scanner;
 
-public class Task32 {
+public class Task32_2 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         int n = console.nextInt();
-        String x = "1";
+        int x = 1;
+
         for (int i = 1; i < n; i++) {
-            x += "0";
+            x *= 10;
         }
         System.out.println(x);
     }
 }
-
-// подумать: на выходе должно быть число, а не строка (без строк, без parseint)
