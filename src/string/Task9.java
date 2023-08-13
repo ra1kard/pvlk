762a78e8dd8b20ee55a7195137abc6259baea1b8
ra1kardFromMacOSDaria <91raykard@gmail.com>
2023-08-13 12:08:57 +0300

    task9 is done

---
+++ src/string/Task9.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String str = console.nextLine();
+        int n = console.nextInt();
+        char[] chArray= str.toCharArray();
+        String newStr = "##";
+        int count = 1;
+
+        for (int i = 1; i < chArray.length; i++) {
+            if (count < n) {
+                newStr += chArray[i];
+                count++;
+            } else {
+                newStr += "##";
+                count = 1;
+            }
+        }
+
+        System.out.println(newStr);
+    }
+}
