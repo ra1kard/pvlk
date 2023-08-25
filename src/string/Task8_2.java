70ce1da589d01ff0d1dffe0f0d8a62304035d7bf
ra1kard_air <91raykard@gmail.com>
2023-08-25 20:14:38 +0300

    task8 refactor

--- src/string/Task8_2.java
+++ src/string/Task8_2.java
@@ -7,10 +7,12 @@ public class Task8_2 {
         Scanner console = new Scanner(System.in);
         String str = console.nextLine();
         int n = console.nextInt();
-        int count = 0;
         char[] chArray= str.toCharArray();
 
-        //не понимаю причем здесь четность нечетность и как это может помочь...
-        //тут надо же считать когда попадаем на число введенное пользователем и при попадании менять
+        for (int i = 0; i < chArray.length; i++) {
+            if (i % n == 0) chArray[i] = '#';
+        }
+
+        System.out.println(String.valueOf(chArray));
     }
 }
