ca827b7275622156428f6372f09aa0b490a800ca
ra1kard_air <91raykard@gmail.com>
2023-08-25 18:46:34 +0300

    task9 refactor

--- src/string/Task9_2.java
+++ src/string/Task9_2.java
@@ -2,22 +2,19 @@ package string;
 
 import java.util.Scanner;
 
-public class Task9 {
+public class Task9_2 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         String str = console.nextLine();
         int n = console.nextInt();
         char[] chArray= str.toCharArray();
         String newStr = "##";
-        int count = 1;
 
         for (int i = 1; i < chArray.length; i++) {
-            if (count < n) {
-                newStr += chArray[i];
-                count++;
-            } else {
+            if (i % n == 0) {
                 newStr += "##";
-                count = 1;
+            } else {
+                newStr += chArray[i];
             }
         }
 
