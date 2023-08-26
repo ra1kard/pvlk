e07f19ec936f4a8be706033e1f924f114695d54d
ra1kard_air <91raykard@gmail.com>
2023-08-27 01:51:59 +0300

    task68 refactor

---
+++ src/loop/Task68_3.java
@@ -0,0 +1,23 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Task68_3 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int x = console.nextInt();
+        int i = x;
+        int value;
+        int rsl = 0;
+
+        while (i > 0) {
+            value = i % 10;
+            i = i / 10;
+            if (value != 0) {
+                rsl += value * value * value;
+            }
+        }
+
+        System.out.println((x == rsl) ? "Число Армстронга" : "Не число Армстронга");
+    }
+}
