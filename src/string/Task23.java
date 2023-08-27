648652d69dff79db42a25a699620b58b91c2c507
ra1kard_air <91raykard@gmail.com>
2023-08-27 13:43:06 +0300

    task12 refactor

--- src/string/Task23.java
+++ src/string/Task23.java
@@ -6,23 +6,23 @@ public class Task23 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         String str1 = console.nextLine();
-        char[] chArray1 = str1.toCharArray();
+        char[] chArray = str1.toCharArray();
         String str2 = null;
 
-        for (int i = 0; i < chArray1.length; i++) {
-            if (chArray1[i] == '*') {                    //если попали на символ
+        for (int i = 0; i < chArray.length; i++) {
+            if (chArray[i] == '*') {                    //если попали на символ
                 int j = 1;
                 int count = 1;
-                while (chArray1[i+j] == '*') {           //посчитаем сколько у нас подряд символов = 42
+                while (chArray[i+j] == '*') {           //посчитаем сколько у нас подряд символов = 42
                     j++;
                     count++;                            //счетчик символов подряд
                 }
                 i += count + (count-1);                 //затем плюсуем кол-во звездочек и кол-во пропускаемы букв
             } else {
                 if (str2 == null) {
-                    str2 = String.valueOf(chArray1[i]);
+                    str2 = String.valueOf(chArray[i]);
                 } else {
-                    str2 += chArray1[i];
+                    str2 += chArray[i];
                 }
             }
         }
@@ -30,4 +30,4 @@ public class Task23 {
         System.out.println(str2);
     }
 }
-//вычислить, является ли последней звездой (сократит while)
+// вычислить, является ли последней звездой (сократит while)
