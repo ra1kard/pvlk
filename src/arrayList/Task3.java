8552542a4e5853d9da0c934d03af9a763ac9feb9
ra1kard_air <91raykard@gmail.com>
2023-09-03 18:09:37 +0300

    task3 refactor

--- src/arrayList/Task3.java
+++ src/arrayList/Task3.java
@@ -9,15 +9,15 @@ public class Task3 {
         //ArrayList<Integer> list = new ArrayList<>(List.of(10, 20, 30));
         //ArrayList<Integer> list = new ArrayList<>(List.of(10));
         //ArrayList<Integer> list = new ArrayList<>(List.of(20, 20));
-        int longOrder = 0;
-        int shortOrder = 0;
+        int maxOrder = 0;
+        int minOrder = 0;
 
         for (int i = 0; i < list.size()-1; i++) {
-            longOrder = list.get(i+1) > list.get(longOrder) ? i+1 : longOrder;
-            shortOrder = list.get(i+1) <= list.get(shortOrder) ? i+1 : shortOrder;
+            maxOrder = list.get(i+1) > list.get(maxOrder) ? i+1 : maxOrder;
+            minOrder = list.get(i+1) <= list.get(minOrder) ? i+1 : minOrder;
         }
 
-        System.out.println("Минимальный элемент: " + list.get(shortOrder));
-        System.out.println("Максимальный элемент: " + list.get(longOrder));
+        System.out.println("Минимальный элемент: " + list.get(minOrder));
+        System.out.println("Максимальный элемент: " + list.get(maxOrder));
     }
 }
