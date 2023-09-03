4db04ced879b5dd0d92e37737e5901a3411e2e9b
ra1kard_air <91raykard@gmail.com>
2023-09-03 16:50:45 +0300

    task1 refactor

--- src/arrayList/Task1_2.java
+++ src/arrayList/Task1_2.java
@@ -7,10 +7,11 @@ public class Task1_2 {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>(List.of("Трое", "в", "лодке", "не", "считая", "собаки"));
         System.out.println(list);
-        for (String s : list) {
-            if (s.length() > 5) {
-                System.out.println(s);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).length() > 5) {
+                System.out.println(list.get(i));
             }
         }
     }
 }
+//это наиболее оптимальный вариант??? или 1_2??? потому что тут мы не переводим в String
--- src/arrayList/Task1_3.java
+++ src/arrayList/Task1_3.java
@@ -3,15 +3,14 @@ package arrayList;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Task1_1 {
+public class Task1_3 {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>(List.of("Трое", "в", "лодке", "не", "считая", "собаки"));
         System.out.println(list);
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).length() > 5) {
-                System.out.println(list.get(i));
+        for (String s : list) {
+            if (s.length() > 5) {
+                System.out.println(s);
             }
         }
     }
 }
-//это наиболее оптимальный вариант??? или 1_2??? потому что тут мы не переводим в String
