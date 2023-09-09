b41b9bd858f98f911348db33b40cd10e83a8758b
ra1kard_air <91raykard@gmail.com>
2023-09-10 02:24:58 +0300

    refactor

--- src/arrayList/Task3.java
+++ src/arrayList/Task3.java
@@ -5,19 +5,19 @@ import java.util.List;
 
 public class Task3 {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>(List.of(100, 20, 3));
+        ArrayList<Integer> list = new ArrayList<>(List.of(100, 20, 300));
         //ArrayList<Integer> list = new ArrayList<>(List.of(10, 20, 30));
         //ArrayList<Integer> list = new ArrayList<>(List.of(10));
         //ArrayList<Integer> list = new ArrayList<>(List.of(20, 20));
-        int maxOrder = 0;
-        int minOrder = 0;
+        int maxIndex = 0;
+        int minIndex = 0;
 
-        for (int i = 0; i < list.size()-1; i++) {
-            maxOrder = list.get(i+1) > list.get(maxOrder) ? i+1 : maxOrder;
-            minOrder = list.get(i+1) <= list.get(minOrder) ? i+1 : minOrder;
+        for (int i = 1; i < list.size(); i++) {
+            maxIndex = list.get(i) > list.get(maxIndex) ? i : maxIndex;
+            minIndex = list.get(i) <= list.get(minIndex) ? i : minIndex;
         }
 
-        System.out.println("Минимальный элемент: " + list.get(minOrder));
-        System.out.println("Максимальный элемент: " + list.get(maxOrder));
+        System.out.println("Минимальный элемент: " + list.get(minIndex));
+        System.out.println("Максимальный элемент: " + list.get(maxIndex));
     }
 }
