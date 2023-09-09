8a074007dc42e222e9fe8a341e57e6d422fbc73b
ra1kard_air <91raykard@gmail.com>
2023-09-10 02:23:51 +0300

    refactor

--- src/arrayList/Task2.java
+++ src/arrayList/Task2.java
@@ -8,11 +8,17 @@ public class Task2 {
         ArrayList<String> list = new ArrayList<>(List.of("один", "шестььь", "восемь"));
         String biggest = list.get(0);
 
-        for (int i = 1; i < list.size(); i++) {
+        /*for (int i = 1; i < list.size(); i++) {
             if (list.get(i).length() > biggest.length()) {
                 biggest = list.get(i);
+            }
+        }*/
+        for (String s : list) {
+            if (s.length() > biggest.length()) {
+                biggest = s;
             }
         }
         System.out.println(biggest);
     }
 }
+//лучшее решение из 4 вариантов
