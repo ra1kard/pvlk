475f8e11735a1a167de99401d0f1be98ee76c918
ra1kard_air <91raykard@gmail.com>
2023-09-10 02:56:54 +0300

    task8 is done

---
+++ src/arrayList/Task8_2.java
@@ -0,0 +1,25 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Task8_2 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>(List.of("Молоко", "Сметана", "Хлеб",
+                "Кетчуп", "Майонез", "Конфеты"));
+
+        Scanner console = new Scanner(System.in);
+        String str = console.nextLine();
+        boolean available = false;
+
+        for (String s : list) {
+            if (str.equalsIgnoreCase(s)) {
+                available = true;
+                break;
+            }
+        }
+
+        System.out.println(available ? str + " есть в наличии" : str + " отсутствует");
+    }
+}
