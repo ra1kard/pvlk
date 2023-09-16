a35310ebf9d44680ce911259953faf7b2d65bf7e
ra1kard_air <91raykard@gmail.com>
2023-09-17 02:45:25 +0300

    task17 is done

---
+++ src/arrayList/Task17.java
@@ -0,0 +1,28 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task17 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(List.of(10, 0, 15, 20, 4, 0, 15, 0, 9, 0, 1));
+        //ArrayList<Integer> list = new ArrayList<>(List.of(15, 27, 9, 0, 14, 6, 28, -7, 0));
+        //ArrayList<Integer> list = new ArrayList<>(List.of(10, 1, 2, 3));
+        int rsl = 0;
+        int countNull = 0;
+
+        if (list.contains(0)) {
+            for (int l : list) {
+                if (l == 0) {
+                    countNull++;
+                } else if (countNull == 1) {
+                    rsl += l;
+                } else if (countNull > 1) {
+                    break;
+                }
+            }
+        }
+
+        System.out.println(countNull > 1 ? rsl : 0);
+    }
+}
