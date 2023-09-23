6a5ce5e323b1f8f0f009f19ef645f88e75828b63
ra1kard_air <91raykard@gmail.com>
2023-09-24 02:05:22 +0300

    ref

--- src/arrayList/Task15.java
+++ src/arrayList/Task15.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Task15 {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>(List.of("Bob D", "Zoz", "Ror"));
+        ArrayList<String> list = new ArrayList<>(List.of("vob D", "Zoz", "ror"));
         boolean smallWords = false;
 
         for (String str : list) {
