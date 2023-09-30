c3cc41ab8276189f06ca14b205d6b30d10675487
ra1kard_air <91raykard@gmail.com>
2023-09-30 16:48:24 +0300

    task24 refactor

--- src/arrayList/Task24.java
+++ src/arrayList/Task24.java
@@ -7,6 +7,7 @@ public class Task24 {
     public static void main(String[] args) {
         //ArrayList<Integer> list = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8));
         ArrayList<Integer> list = new ArrayList<>(List.of(10, 15, 6, -8, 41, 9, 16, -5, 0));
+        System.out.println(list);
         int lowest = 0;
         int lowestIndex = 0;
         int k = 0;
@@ -29,3 +30,4 @@ public class Task24 {
         System.out.println(lowest);
     }
 }
+//есть еще вариант добавлять в новый список - попробовать (см. task24_2)
