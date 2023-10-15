cbc426d25bdbdb8c28c92f6888aaa50278281db2
ra1kard_air <91raykard@gmail.com>
2023-10-15 20:46:18 +0300

    task3 refactor

--- src/hashSet/Task3.java
+++ src/hashSet/Task3.java
@@ -7,17 +7,19 @@ public class Task3 {
         /*HashSet<String> states = new HashSet<String>(Arrays.asList("Germany", "France", "Italy"));
         System.out.println(states);*/
 
-        HashSet<String> states = new HashSet<String>();
+        HashSet<String> states = new HashSet<>();
+        states.add("Ital1");
         states.add("Germany");
-        states.add("France");
         states.add("Italy");
-        System.out.println(states);             // [Italy, France, Germany] последний добавленный первый выходит?
+        states.add("France");
+        states.add("Ital2");
+        states.add("Ital3");
+        System.out.println(states);
 
-        /*
-        выведите первый элемент набора                  а что вообще есть первый элемент в HashSet? он же хаос?
-        Для получения элемента из Set в Java можно использовать метод Iterator.next(),
-        который возвращает следующий элемент из набора.
-         */
+        //выведите первый элемент набора (имеется ввиду тот первый, что записался в Set)
+
+        //Для получения элемента из Set в Java можно использовать метод Iterator.next(),
+        //который возвращает следующий элемент из набора.
         Iterator<String> iterator = states.iterator();
         String element = iterator.next();
         System.out.println(element);                    // Italy - вариант 1
@@ -28,6 +30,6 @@ public class Task3 {
         System.out.println(list.get(0));                // преобразовать HashSet в ArrayList - вариант 3
 
         //выведите последний элемент набора
-        System.out.println(list.get(list.size()-1));    // преобразовать HashSet в ArrayList (а как еще?)
+        System.out.println(list.get(list.size()-1));    // преобразовать HashSet в ArrayList
     }
 }
