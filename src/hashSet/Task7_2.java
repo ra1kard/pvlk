8a262f30b97387992d1d065bc24dfdd092c24553
ra1kard_air <91raykard@gmail.com>
2023-10-22 23:40:14 +0300

    refactor

--- src/hashSet/Task7_2.java
+++ src/hashSet/Task7_2.java
@@ -2,21 +2,17 @@ package hashSet;
 
 import java.util.*;
 
-public class Task7 {
+public class Task7_2 {
     public static void main(String[] args) {
         HashSet<Integer> numbersOne = new HashSet<>(Arrays.asList(10, 90, 30, 40, 50, 60));
         HashSet<Integer> numbersTwo = new HashSet<>(Arrays.asList(11, 90, 33, 40, 55, 60));
-        List<Integer> list = new ArrayList<>();
-
+        TreeSet<Integer> numbersTree = new TreeSet<>();
 
         for (Integer i : numbersOne) {
             if (numbersTwo.contains(i)) {
-                list.add(i);
+                numbersTree.add(i);
             }
         }
-        System.out.println(list);
-
-        TreeSet<Integer> numbersTree = new TreeSet<>(list);
         System.out.println(numbersTree);
     }
 }
