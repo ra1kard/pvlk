375e9563e697fc0cbbb5aac16ef3e3daedb7b31c
ra1kard_air <91raykard@gmail.com>
2023-12-25 18:36:34 +0300

    Task20 is done

--- src/method/Task19.java
+++ src/method/Task19.java
@@ -2,22 +2,25 @@ package method;
 
 public class Task19 {
     public static void main(String[] args) {
-        System.out.println(myMethod(153) ? "Число Армстронга" : "Не число Армстронга");
-        System.out.println(myMethod(370) ? "Число Армстронга" : "Не число Армстронга");
-        System.out.println(myMethod(123) ? "Число Армстронга" : "Не число Армстронга");
-        System.out.println(myMethod(8) ? "Число Армстронга" : "Не число Армстронга");
-        System.out.println(myMethod(36) ? "Число Армстронга" : "Не число Армстронга");
-        System.out.println(myMethod(8208) ? "Число Армстронга" : "Не число Армстронга");
+        System.out.println(checkArmstrong(153) ? "Число Армстронга" : "Не число Армстронга");
+        System.out.println(checkArmstrong(370) ? "Число Армстронга" : "Не число Армстронга");
+        System.out.println(checkArmstrong(123) ? "Число Армстронга" : "Не число Армстронга");
+        System.out.println(checkArmstrong(8) ? "Число Армстронга" : "Не число Армстронга");
+        System.out.println(checkArmstrong(36) ? "Число Армстронга" : "Не число Армстронга");
+        System.out.println(checkArmstrong(8208) ? "Число Армстронга" : "Не число Армстронга");
     }
 
-    public static boolean myMethod(int value) {
-        String str = String.valueOf(value);
-        char[] array = str.toCharArray();
-        int rsl = 0;
-        for (char c : array) {
-            int num = Character.getNumericValue(c);
-            rsl += Math.pow(num, array.length);
+    public static boolean checkArmstrong(int value) {
+        if (value > 0) {
+            String str = String.valueOf(value);
+            char[] array = str.toCharArray();
+            int rsl = 0;
+            for (char c : array) {
+                int num = Character.getNumericValue(c);
+                rsl += Math.pow(num, array.length);
+            }
+            return (value == rsl);
         }
-        return (value == rsl);
+        return false;
     }
 }
