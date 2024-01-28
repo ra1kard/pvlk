eed01551bae287d36dfbfdbc3708a007263a9ff1
ra1kard_air <91raykard@gmail.com>
2024-01-28 17:03:20 +0300

    Task1

--- src/classes/task1/AdvancedMath.java
+++ src/classes/task1/AdvancedMath.java
@@ -1,24 +1,6 @@
-package classes.task2;
+package classes.task1;
 
 public class AdvancedMath {
-
-    public static void main(String[] args) {
-        System.out.println("проверка простоты числа 13 " + isPrime(13));
-        System.out.println("проверка простоты числа 10 " + isPrime(10));
-        System.out.println();
-
-        System.out.println("полиндром 121? = " + isPalindrome(121));
-        System.out.println("полиндром 123? = " + isPalindrome(123));
-        System.out.println();
-
-        System.out.println("четное 22? = " + isEven(22));
-        System.out.println("четное 21? = " + isEven(21));
-        System.out.println();
-
-        System.out.println("нечетное 5? = " + isOdd(5));
-        System.out.println("нечетное 6? = " + isOdd(6));
-        System.out.println();
-    }
 
     /** проверка простоты числа
      *  Простое число - это натуральное число (>0),
--- src/classes/task1/Start.java
+++ src/classes/task1/Start.java
@@ -1,74 +1,25 @@
 package classes.task1;
 
-import java.util.ArrayList;
-import java.util.List;
+import static classes.task1.AdvancedMath.*;
 
 public class Start {
 
     public static void main(String[] args) {
-        ArrayList<Person> list = new ArrayList<>();
-        list.add(new Person());
-        list.add(new Person("Станислав", "Иванов", 32));
-        list.add(new Person("Кристина", "Крутова", 35));
-        list.add(new Person("Афродита", "Поподопулос", 46));
-        list.add(new Person("Настасья", "Креймер", 27));
+        System.out.println("проверка простоты числа 13 " + isPrime(13));
+        System.out.println("проверка простоты числа 10 " + isPrime(10));
+        System.out.println();
 
-        youngest(list);             //тут просит сделать статичным обязательно, почему?
-        System.out.println("Самый молодой - " + list.get(youngest(list)).getSurname());
+        System.out.println("полиндром 121? = " + isPalindrome(121));
+        System.out.println("полиндром 123? = " + isPalindrome(123));
+        System.out.println();
 
-        System.out.println("Самый старый - " + list.get(oldest(list)).getSurname());
+        System.out.println("четное 22? = " + isEven(22));
+        System.out.println("четное 21? = " + isEven(21));
+        System.out.println();
 
-        System.out.println("У кого фамилии на \"К\": ");
-        for (Person person : surnameK(list)) {
-            System.out.println(person.getSurname());
-        }
-
-        list.get(1).move(10500);
-        list.get(2).talk("всех приветствую, меня зовут - " + list.get(2).getName());
-    }
-
-    //самый молодой
-    public static int youngest(ArrayList<Person> list) {
-        int youngest = Integer.MAX_VALUE;   //самый молодой
-        int youngestIndex = 0;
-        /*for (Person person : list) {
-            if (person.getAge() < youngest) {
-                youngest = person.getAge();
-            }
-        }*/
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getAge() < youngest) {
-                youngest = list.get(i).getAge();
-                youngestIndex = i;
-            }
-        }
-        return youngestIndex;
-    }
-
-    //самый старый
-    public static int oldest(ArrayList<Person> list) {
-        int oldest = list.get(0).getAge();
-        int oldestIndex = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getAge() > oldest) {
-                oldest = list.get(i).getAge();
-                oldestIndex = i;
-            }
-        }
-        return oldestIndex;
-    }
-
-    //все люди с фамилией на букву "К"
-    public static List<Person> surnameK(ArrayList<Person> list) {
-        ArrayList<Person> listSurnameK = new ArrayList<>();
-
-        for (Person person : list) {
-            if (person.getSurname().charAt(0) == 'К') {
-                listSurnameK.add(person);
-            }
-        }
-
-        return listSurnameK;
+        System.out.println("нечетное 5? = " + isOdd(5));
+        System.out.println("нечетное 6? = " + isOdd(6));
+        System.out.println();
     }
 
 }
