4085d3c2aac8bdd1095c1916a1292c37294c25fa
ra1kard_air <91raykard@gmail.com>
2024-01-29 02:24:21 +0300

    task6 is done

--- src/classes/task6/Herbivore.java
+++ src/classes/task6/Herbivore.java
@@ -3,7 +3,7 @@ package classes.task6;
 public class Herbivore {        //травоядное животное
     private String name;
     private int kind;           //вид животного (1мал, 2сред, 3бол)
-    private int satiety = 50;   //сытость (100 - полностью сытая, 0 - смертельно голодная)
+    private int satiety;   //сытость (100 - полностью сытая, 0 - смертельно голодная)
 
     Herbivore(String name, int kg, int satietyFrom0To100) {
         this.name = name;
@@ -48,13 +48,16 @@ public class Herbivore {        //травоядное животное
         System.out.println(grass.getNutrition() + " nutrition питательность травы");
         System.out.println(Herbivore.this.getSatiety() + " satiety сытость животного");
         while (satiety < 50) {                              // 1)сыто < 50%
-            if (grass.getNutrition() > 10) {                // 2)есть что есть
+            if (grass.getNutrition() > 0) {                // 2)есть что есть
                 System.out.print("Приступаю к еде! ");
                 grass.setNutrition(grass.getNutrition()-10);
                 this.satiety = this.satiety + 10;
                 System.out.print(Herbivore.this.getSatiety() + " satiety сытость животного, ");
                 System.out.println(grass.getNutrition() + " nutrition питательность травы");
             }
+        }
+        if (grass.getNutrition() <= 0) {
+            System.out.println("Травы не осталось");
         }
         if (satiety >= 50) {
             System.out.println("Не хочет есть");
