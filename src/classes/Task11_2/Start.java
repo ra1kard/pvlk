5895e68cacf34f5eac20cde7fe7bdbe373c214f4
ra1kard_air <91raykard@gmail.com>
2024-02-26 01:16:56 +0300

    ref

--- src/classes/Task11_2/Start.java
+++ src/classes/Task11_2/Start.java
@@ -12,7 +12,7 @@ public class Start {
         ArrayList<Worker> workers = new ArrayList<>(List.of(aleksey, nikolay, vasiliy));
 
         aleksey.setTasks(new Task(1000, Difficult.MIDDLE, "Заменить окна в квартире"));
-        aleksey.setTasks(new Task(1001, Difficult.HARD__, "Покраска стен в квартире"));
+        nikolay.setTasks(new Task(1001, Difficult.HARD__, "Покраска стен в квартире"));
         nikolay.setTasks(new Task(1002, Difficult.HARD__, "Выравнивание пола в квартире"));
         nikolay.setTasks(new Task(1003, Difficult.HARD__, "Установить скрытые двери в комнаты"));
         vasiliy.setTasks(new Task(1004, Difficult.MIDDLE, "Заменить входную дверь в квартире"));
@@ -36,6 +36,8 @@ public class Start {
         printMostCountDifficultTask(workers);
         
         printMostCostTasks(workers);
+
+        whoWorkerWithoutTasks(workers);
     }
 
     public static void printMostCountDifficultTask(ArrayList<Worker> workers) {
@@ -82,5 +84,31 @@ public class Start {
         }
         assert workerMostCost != null;
         System.out.println("Рабочий с наибольшей стоимостью задач: " + workerMostCost.getName());
+        System.out.println();
     }
+
+    public static void whoWorkerWithoutTasks(ArrayList<Worker> workers) {
+        ArrayList<Worker> workersWithoutTasks = new ArrayList<>();
+        for (Worker worker : workers) {
+            int count = 0;
+            for (Difficult difficultCurrent : worker.getTasks().keySet()) {
+                if (worker.getTasks().get(difficultCurrent).size() != 0) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                workersWithoutTasks.add(worker);
+            }
+        }
+        if (workersWithoutTasks.size() > 0) {
+            System.out.println("Список сотрудников без задач:");
+            for (Worker workersWithoutTask : workersWithoutTasks) {
+                System.out.println(workersWithoutTask.getName());
+            }
+        } else {
+            System.out.println("Сотрудников без задач не нашлось");
+        }
+        System.out.println();
+    }
+
 }
