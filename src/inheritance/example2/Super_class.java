b8a8880c32fae2474a857c1a53ff67ef6c63bb24
ra1kard_air <91raykard@gmail.com>
2024-03-11 20:12:16 +0300

    ref

---
+++ src/inheritance/example2/Super_class.java
@@ -0,0 +1,13 @@
+package inheritance.example2;
+
+public class Super_class {
+    int age;
+
+    Super_class(int age) {
+        this.age = age;
+    }
+
+    public void getAge() {
+        System.out.println("Значение переменной age в суперклассе равно: " + age);
+    }
+}
