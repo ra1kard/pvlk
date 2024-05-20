3558fbe4ec51fce22b922c2417cd36fcb74853e4
ra1kard_air <91raykard@gmail.com>
2024-05-20 03:05:58 +0300

    task7 is done

---
+++ src/interfaces/task7/Tank.java
@@ -0,0 +1,41 @@
+package interfaces.task7;
+
+public class Tank implements Transportable {
+    private int costDelivery;
+    private int width;
+    private int height;
+    private int length;
+
+    Tank(int costDelivery, int width, int height, int length) {
+        this.costDelivery = costDelivery;
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    @Override
+    public int costDelivery() {
+        return costDelivery;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public String toString() {
+        return "Танк";
+    }
+
+}
