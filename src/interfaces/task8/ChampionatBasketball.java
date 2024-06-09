78927b76a5f18d73f71c3fbc0c854c9ef29e864d
ra1kard_air <91raykard@gmail.com>
2024-06-09 20:04:19 +0300

    ref

--- src/interfaces/task8/ChampionatBasketball.java
+++ src/interfaces/task8/ChampionatBasketball.java
@@ -5,9 +5,8 @@ import java.util.ArrayList;
 public class ChampionatBasketball extends Championat {
     String nameSport = "Баскетбол";
 
-    ChampionatBasketball(String name, int year, int countConference, int countTeamMembers,
-                         ArrayList<Team> listTeams, int countGamesWithEach) {
-        super(name, year, countConference, countTeamMembers, listTeams, countGamesWithEach);
+    ChampionatBasketball(String name, int year, int countConference, ArrayList<Team> listTeams, int countGamesWithEach) {
+        super(name, year, countConference, listTeams, countGamesWithEach);
     }
 
     public String getNameSport() {
--- src/interfaces/task8/ChampionatHockey.java
+++ src/interfaces/task8/ChampionatHockey.java
@@ -5,9 +5,8 @@ import java.util.ArrayList;
 public class ChampionatHockey extends Championat {
     String nameSport = "Хоккей";
 
-    ChampionatHockey(String name, int year, int countConference, int countTeamMembers,
-                     ArrayList<Team> listTeams, int countGamesWithEach) {
-        super(name, year, countConference, countTeamMembers, listTeams, countGamesWithEach);
+    ChampionatHockey(String name, int year, int countConference, ArrayList<Team> listTeams, int countGamesWithEach) {
+        super(name, year, countConference, listTeams, countGamesWithEach);
     }
 
     public String getNameSport() {
