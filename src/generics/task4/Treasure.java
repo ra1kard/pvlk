45ad8b17f961da8aad0832b5e63b48db741ed038
ra1kard_air <91raykard@gmail.com>
2024-07-28 21:40:48 +0300

    task4 finish

--- src/generics/task4/Treasure.java
+++ src/generics/task4/Treasure.java
@@ -3,9 +3,9 @@ package generics.task4;
 public abstract class Treasure {    //сокровище - абстрактный родительский класс для бриллиантов и золота
     int quantity;                   //кол-во единиц, например кол-во золотых или кол-во бриллиантов
 
-     Treasure(int quantity) {
+    Treasure(int quantity) {
          this.quantity = quantity;
-     }
+    }
 
     public int getQuantity() {
         return quantity;
